f02c9a594fe6b06f4ccf45e98e029e35fdd857c2
Mujahid <mujahidatar313@gmail.com>
2023-08-23 00:17:04 +0530

    Sohail: CORS enabled

--- src/main/java/TaskManagementSystem/security/SecurityConfig.java
+++ src/main/java/TaskManagementSystem/security/SecurityConfig.java
@@ -3,7 +3,6 @@ package TaskManagementSystem.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -14,6 +13,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 import TaskManagementSystem.service.UserDetailsServiceImpl;
 
@@ -35,6 +35,8 @@ public class SecurityConfig{
 		http.
 			csrf()
 			.disable()
+			.cors()
+			.and()
 			.authorizeHttpRequests()
 			.antMatchers("/login/authenticate")
 			.permitAll()
