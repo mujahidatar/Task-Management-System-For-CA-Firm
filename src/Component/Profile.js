89c10cc0d3aa4e99a93116baad4d7787f5cf6b02
Mujahid <mujahidatar313@gmail.com>
2023-08-22 18:07:59 +0530

    Sohail: profile updated

--- src/Component/Profile.js
+++ src/Component/Profile.js
@@ -54,7 +54,7 @@ const Profile = () => {
                 </div>
             ),
         });
-        
+
         if (temp === "employee") {
             var mydata = {
                 empId: id,
@@ -76,9 +76,9 @@ const Profile = () => {
                 clientPassword: password
             }
         }
-          
 
-       
+
+
 
     }
 
@@ -112,96 +112,103 @@ const Profile = () => {
 
     return (
         <div >
-                <div className="container py-5" style={{alignItems:"center"}}>
-                   
-                        <div className="col-lg-10" >
-                            <div className="card mb-4">
-                                <div className="card-body text-center">
-                                    <h5 className="my-3">{myuser?.empName || myuser?.clientName}</h5>
-                                    <p className="text-muted mb-1">({user.role})</p>
+            <div className="container py-1  align-items-center justify-content-center">
 
-                                </div>
-                            </div>
+                <div className="col-lg-12  align-items-center justify-content-center text-center" >
+                    <div className="card mb-2">
+                        <div className="card-body text-center">
+                            <h5 className="my-0">{myuser?.empName || myuser?.clientName}</h5>
+                            <p className="text-muted mb-0">({user.role})</p>
+
                         </div>
-                        <div className="col-lg-10">
-                            <div className="card mb-4">
-                                <div className="card-body">
-                                    <form onSubmit={handleUpdate}>
-                                        <div className="row">
-                                            <div className="col-sm-3">
-                                                <p className="mb-0">Id</p>
-                                            </div>
-                                            <div className="col-sm-9">
-                                                <input className="text-muted mb-0" value={myuser?.empId || myuser?.clientId}></input>
-                                            </div>
-                                        </div>
-                                        <hr />
-                                        <div className="row">
-                                            <div className="col-sm-3">
-                                                <p className="mb-0"  >Full Name</p>
-                                            </div>
-                                            <div className="col-sm-9">
-                                                <input className="text-muted mb-0" value={myuser?.empName || myuser?.clientName}></input>
-                                            </div>
-                                        </div>
-                                        <hr />
-                                        <div className="row">
-                                            <div className="col-sm-3">
-                                                <p className="mb-0">Email</p>
-                                            </div>
-                                            <div className="col-sm-9">
-                                                <input className="text-muted mb-0" value={myuser?.empEmail || myuser?.clientEmail}></input>
-                                            </div>
-                                        </div>
-                                        <hr />
-                                        <div className="row" hidden={temp === "client"}>
-                                            <div className="col-sm-3">
-                                                <p className="mb-0">Role</p>
-                                            </div>
-                                            <div className="col-sm-9">
-                                                <input className="text-muted mb-0" value={myuser?.empRole}></input>
-                                            </div>
-                                        </div>
-                                        <hr hidden={temp === "client"} />
-                                        <div className="row" hidden={temp === "client" || myuser?.empRole !== "EMPLOYEE"}>
-                                            <div className="col-sm-3">
+                    </div>
+                </div>
+                <div className="col-lg-12">
+                    <div className="card">
+                        <div className="card-body">
+                            <form onSubmit={handleUpdate}>
+                                <div className="row justify-content-center">
+                                    <div className="col-sm-1">
+                                        <p className="mb-0">Id</p>
+                                    </div>
+                                    <div className="col-sm-2">
+                                        <input className="text-muted mb-0" value={myuser?.empId || myuser?.clientId}></input>
+                                    </div>
+                                </div>
+                                <hr />
+                                <div className="row justify-content-center">
+                                    <div className="col-sm-1">
+                                        <p className="mb-0"  >Name</p>
+                                    </div>
+                                    <div className="col-sm-2">
+                                        <input className="text-muted mb-0" value={myuser?.empName || myuser?.clientName}></input>
+                                    </div>
+                                </div>
+                                <hr />
+                                <div className="row justify-content-center">
+                                    <div className="col-sm-1">
+                                        <p className="mb-0">Email</p>
+                                    </div>
+                                    <div className="col-sm-2">
+                                        <input className="text-muted mb-0" value={myuser?.empEmail || myuser?.clientEmail}></input>
+                                    </div>
+                                </div>
+                                <hr />
+                                <div className="row justify-content-center" hidden={temp === "client"}>
+                                    <div className="col-sm-1">
+                                        <p className="mb-0">Role</p>
+                                    </div>
+                                    <div className="col-sm-2">
+                                        <input className="text-muted mb-0" value={myuser?.empRole}></input>
+                                    </div>
+                                </div>
+                                <hr hidden={temp === "client"} />
+                                {
+                                    (temp !== "client" && myuser?.empRole === "EMPLOYEE") &&
+                                    <>
+                                        <div className="row justify-content-center">
+                                            <div className="col-sm-1">
                                                 <p className="mb-0">Manager ID</p>
                                             </div>
-                                            <div className="col-sm-9">
+                                            <div className="col-sm-2">
                                                 <input className="text-muted mb-0" value={myuser?.managerId}></input>
                                             </div>
                                         </div>
-                                        <hr hidden={temp === "client" || myuser?.empRole !== "EMPLOYEE"} />
-                                        <div className="row">
-                                            <div className="col-sm-3">
-                                                <p className="mb-0">Phone</p>
-                                            </div>
-                                            <div className="col-sm-9">
-                                                <input name="name" value={number} onChange={(e) => setNumber(e.target.value)} />
-                                            </div>
-                                        </div>
                                         <hr />
+                                    </>
 
-                                        <div className="row">
-                                            <div className="col-sm-3">
-                                                <p className="mb-0" >Address</p>
-                                            </div>
-                                            <div className="col-sm-9">
-                                                <input name="name" value={address} onChange={(e) => setAddress(e.target.value)} />
-                                            </div>
-                                        </div>
-                                        <hr />
-                                        <div className="row mb-3 pt-3">
-                                            <div className="col-sm-10" style={{ "margin": "auto", "width": 360 }}>
-                                                <input type="submit" className="form-control btn btn-info" value="Update" style={{ "fontWeight": "bold" }} />
-                                            </div>
-                                        </div>
-                                    </form>
+                                }
+
+                                <div className="row justify-content-center">
+                                    <div className="col-sm-1">
+                                        <p className="mb-0">Phone</p>
+                                    </div>
+                                    <div className="col-sm-2">
+                                        <input name="name" value={number} onChange={(e) => setNumber(e.target.value)} />
+                                    </div>
                                 </div>
-                            </div>
+                                <hr />
 
+                                <div className="row justify-content-center">
+                                    <div className="col-sm-1">
+                                        <p className="mb-0" >Address</p>
+                                    </div>
+                                    <div className="col-sm-2">
+                                        <input name="name" value={address} onChange={(e) => setAddress(e.target.value)} />
+                                    </div>
+                                </div>
+                                <hr />
+                                <div className="row mb-1 pt-1">
+                                    <div className="col-sm-6" style={{ "margin": "auto", "width": 360 }}>
+                                        <input type="submit" className="form-control btn btn-info" value="Update" style={{ "fontWeight": "bold" }} />
+                                    </div>
+                                </div>
+                            </form>
                         </div>
+                    </div>
+
                 </div>
+            </div>
         </div>
     )
 }
